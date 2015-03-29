c89a97bfc3fd6cd782fe7cf90587b0ae2688a8f4
Chris <chrispy004@gmail.com>
2015-03-28 17:14:24 -0700

    start flash

---
+++ lib/phase4/session.rb
@@ -0,0 +1,30 @@
+require 'json'
+require 'webrick'
+
+module Phase4
+  class Session
+    # find the cookie for this app
+    # deserialize the cookie into a hash
+    def initialize(req)
+      @cookies = {}
+      req.cookies.each do |cookie|
+        if cookie.name == "_rails_lite_app" && !cookie.value.nil?
+          p cookie.value
+          @cookies = JSON.parse(cookie.value)
+        end
+      end
+    end
+
+    def [](key)
+      @cookies[key]
+    end
+
+    def []=(key, val)
+      @cookies[key] = val
+    end
+
+    def store_session(res)
+        res.cookies << WEBrick::Cookie.new("_rails_lite_app", @cookies.to_json)
+    end
+  end
+end
